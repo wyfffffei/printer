7bb9c138143851870c66e85598fb4308ec81d2d3
wyfffffei <h3wyf@126.com>
2023-02-20 11:23:15 +0800

    add padding with random color

--- utils/util_image.py
+++ utils/util_image.py
@@ -18,7 +18,7 @@ def rotate_pic(img):
 
 def padding_pic(img):
     """
-    @ func @  add padding from rectangle to square picture (w > h)
+    @ func: add padding from rectangle to square picture (w > h)
     """
     w, h = img.size
     if w <= h:
@@ -29,6 +29,31 @@ def padding_pic(img):
     img_array = np.rot90(img_padded, 1)                     # 逆时针旋转矩阵 90 度
     img = Image.fromarray(img_array)                        # 矩阵转换回 Pillow 对象
     return img
+
+
+def more_padding_pic(img):
+    """
+    @ func: add padding with random color
+    """
+    ori_size = (1024, 1024)
+    new_size = (1366, 768)
+    img.thumbnail(new_size, Image.Resampling.LANCZOS)
+    padding = abs(new_size[0] - new_size[1]) // 2
+
+    # 创建底板
+    img_padded = np.zeros(shape=(new_size[1], new_size[0], 3), dtype=np.uint8)
+    img_padded[:, padding: padding + new_size[1], :] = img
+
+    # 填充颜色
+    # padding_content = 255 * np.ones((new_size[1], padding, 3), dtype=np.uint8)  # 白色
+    padding_content = 255 * np.random.randint((new_size[1], padding, 3))          # 随机颜色
+    
+    # 填充颜色
+    img_padded[:, :padding, :] = padding_content
+    img_padded[:, padding + 768:, :] = padding_content
+    
+    new_img = Image.fromarray(img_padded)
+    return new_img
 
 
 def reg_pic(pictures, path, config):
